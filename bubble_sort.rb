f9402c558cfd7a442ea420efe1d5f15b89f40872
grimyoung <samhyukim@gmail.com>
2017-03-06 21:14:00 -0800

    added method bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -16,5 +16,29 @@ def bubble_sort(array)
 	return array
 end
 
-test = [4,3,78,2,0,2]
-p bubble_sort(test)
+# test = [4,3,78,2,0,2]
+# p bubble_sort(test)
+
+def bubble_sort_by(array)
+	sorted = false
+	numsort = 0
+	until sorted do
+		for i in (0..array.length-2)
+			if yield(array[i],array[i+1]) >1
+				array[i],array[i+1] = array[i+1], array[i]
+				numsort += 1
+			end
+		end
+		if numsort == 0
+			sorted = true
+		end
+		numsort = 0
+	end
+	return array
+end
+
+test = ["hi","hello","hey"]
+p (bubble_sort_by(test) do |left,right|
+	left.length - right.length
+end)
+
