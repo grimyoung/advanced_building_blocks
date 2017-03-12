097038be58cfe70f5951c5482a4a9f1f59338514
grimyoung <samhyukim@gmail.com>
2017-03-11 16:53:42 -0800

    Added method my_count and my_map

--- Enumerable.rb
+++ Enumerable.rb
@@ -54,9 +54,51 @@ module Enumerable
 		return true
 	end
 
+	def my_count(arg = false)
+		num = 0
+		if block_given? && !arg
+			self.my_each do |x|
+				if yield(x)
+					num +=1
+				end
+			end
+		elsif arg
+			self.my_each do |x|
+				if arg == x
+					num += 1
+				end
+			end
+		else
+			self.my_each do |x|
+				num+=1
+			end
+		end
+		return num
+	end
+
+	def my_map
+		array = []
+		self.my_each do |x|
+			array << yield(x)
+		end
+		return array
+	end
+
+	def my_inject(arg = self[0])
+		memo = arg
+		self.my_each do |memo,x|
+			memo = yield(memo,x)
+		end
+		return memo
+	end
+
+	def mutliply_els
+
+	end
+
 	#
-	p [1,2,3].my_none? {|x| x.is_a? Integer}
-	p [1,2,'a'].my_none? {|x| x.is_a? Integer}
-	p [nil,'a',''].my_none? {|x| x.is_a? Integer}
+	p [1,2,3].my_map {|x| x*2}
+	#p [1,2,'a'].my_count {|x| x.is_a? Integer}
+	#p [1,2,2].my_count(2)
 
 end
